bb07b13e16e7e4659a9be642a993d1d1f79a2fc0
Rexsy Bima <Rexsy.Bimq12@gmail.com>
2023-05-23 11:46:08 +0700

    Untuk dipindah ke PC

--- gui.py
+++ gui.py
@@ -2,10 +2,19 @@ import functions
 import PySimpleGUI
 
 label = PySimpleGUI.Text("type in a shopping list")
-input_box = PySimpleGUI.InputText(tooltip="enter shopping list")
+input_box = PySimpleGUI.InputText(tooltip="enter shopping list", key='shopping item')
 add_button = PySimpleGUI.Button("Add")
 
 
-window = PySimpleGUI.Window('My app', layout=[[label, input_box, add_button]])
-window.read()
+window = PySimpleGUI.Window('My shopping list app', 
+                            layout=[[label, input_box, add_button]], 
+                            font=('Helvetica', 20))
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = functions.add_to_lists()
 window.close()
