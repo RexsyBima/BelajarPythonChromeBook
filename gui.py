5e5a354559d70fb57d72707030df7f9690a9cf29
Rexsy Bima <Rexsy.Bimq12@gmail.com>
2023-05-23 09:15:37 +0700

    add cli and gui

---
+++ gui.py
@@ -0,0 +1,11 @@
+import functions
+import PySimpleGUI
+
+label = PySimpleGUI.Text("type in a shopping list")
+input_box = PySimpleGUI.InputText(tooltip="enter shopping list")
+add_button = PySimpleGUI.Button("Add")
+
+
+window = PySimpleGUI.Window('My app', layout=[[label], [input_box, add_button]])
+window.read()
+window.close()
